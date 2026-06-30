179ac57258c2ac17b0ef489abfaad88274c55dcf
Gabriel <gabrielsantana@gmail.com>
2026-06-30 13:09:16 -0300

    pdf server recebe porta como env

--- pdf-server.js
+++ pdf-server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static(__dirname));
 
